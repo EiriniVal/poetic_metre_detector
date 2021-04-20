4df566d90ce20284406c8087c5c8d04dbd2168d6
eirini.valkana <eirini.valkana@uzh.ch>
2021-04-20 10:36:19 +0200

    added tests for function syllabify_verse

--- test_syllabifier.py
+++ test_syllabifier.py
@@ -1,5 +1,5 @@
 import syllabifier as syl
-from unittest import TestCase, main
+from unittest import TestCase
 
 
 class TestSyllabification(TestCase):
@@ -22,31 +22,44 @@ class TestSyllabification(TestCase):
             self.assertEqual(syl.is_monosyllable(word), False, f"The polysyllable word {word} is not recognized as "
                                                                f"polysyllable!")
 
-    def test_output_syllabify_token_while(self):
-        self.assertIsInstance(syl.syllabify_token_while("αεροπλάνο"), str, "Required output type is string!")
+    def test_output_syllabify_token(self):
+        self.assertIsInstance(syl.syllabify_token("αεροπλάνο"), str, "Required output type is string!")
 
-    def test_syllabify_token_while(self):
-        self.assertEqual(syl.syllabify_token_while("αεροπλάνο"), "α ε ρο πλά νο", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("άστρο"), "ά στρο", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("φτάνω"), "φτά νω", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("οχιά"), "ο χιά", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("βοήθα"), "βο ή θα", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("βόηθα"), "βόη θα", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("ευτυχία"), "ευ τυ χί α", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("βαθμούς"), "βαθ μούς", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("φίλτατος"), "φίλ τα τος", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("μυαλωμένος"), "μυα λω μέ νος", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("δουλειά"), "δου λειά", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("δουλεία"), "δου λεί α", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("αργαλειού"), "αρ γα λειού", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("παιδιών"), "παι διών", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("αέναη"), "α έ ναη", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("αηδόνι"), "αη δό νι", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("αΰμνηστος"), "α ΰ μνη στος", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("εκπτώσεις"), "εκ πτώ σεις", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("τριαντάφυλλο"), "τρια ντά φυλ λο", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("πολιορκημένοι"), "πο λιορ κη μέ νοι", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("όμορφος"), "ό μορ φος", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("θάλασσα"), "θά λασ σα", "Syllabification was wrong!")
-        self.assertEqual(syl.syllabify_token_while("ασπίδα"), "α σπί δα", "Syllabification was wrong!")
+    def test_syllabify_token(self):
+        self.assertEqual(syl.syllabify_token("αεροπλάνο"), "α ε ρο πλά νο", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("άστρο"), "ά στρο", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("φτάνω"), "φτά νω", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("οχιά"), "ο χιά", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("βοήθα"), "βο ή θα", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("βόηθα"), "βόη θα", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("ευτυχία"), "ευ τυ χί α", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("βαθμούς"), "βαθ μούς", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("φίλτατος"), "φίλ τα τος", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("μυαλωμένος"), "μυα λω μέ νος", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("δουλειά"), "δου λειά", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("δουλεία"), "δου λεί α", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("αργαλειού"), "αρ γα λειού", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("παιδιών"), "παι διών", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("αέναη"), "α έ ναη", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("αηδόνι"), "αη δό νι", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("αΰμνηστος"), "α ΰ μνη στος", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("εκπτώσεις"), "εκ πτώ σεις", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("τριαντάφυλλο"), "τρια ντά φυλ λο", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("πολιορκημένοι"), "πο λιορ κη μέ νοι", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("όμορφος"), "ό μορ φος", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("θάλασσα"), "θά λασ σα", "Syllabification was wrong!")
+        self.assertEqual(syl.syllabify_token("ασπίδα"), "α σπί δα", "Syllabification was wrong!")
 
+    def test_output_syllabify_verse(self):
+        self.assertIsInstance(syl.syllabify_verse("από αγάλματα και εικόνες"), list, "Required output type is list!")
+        self.assertIsInstance(syl.syllabify_verse("σε γνωρίζω από την κόψη"), list, "Required output type is list!")
+
+    def test_syllabify_verse(self):
+        self.assertEqual(syl.syllabify_verse("από αγάλματα και εικόνες"),
+                         ["α", "πό", "α", "γάλ", "μα", "τα", "και", "ει", "κό", "νες"], "Splitting was wrong")
+        self.assertEqual(syl.syllabify_verse("σε γνωρίζω από την κόψη"),
+                         ["σε", "γνω", "ρί", "ζω", "α", "πό", "την", "κό", "ψη"], "Splitting was wrong")
+        self.assertEqual(syl.syllabify_verse("Την είδα την Ξανθούλα,"),
+                         ["την", "εί", "δα", "την", "ξαν", "θού", "λα,"], "Splitting was wrong")
+        self.assertNotIn("", syl.syllabify_verse("Την είδα την Ξανθούλα,"), "Empty strings are not supposed to be in "
+                                                                            "the list")
